734b4d9f9421e7766dad2f7a6f856811e7c218f1
unknown <myzhang@upc0910.uz.local>
2016-07-20 13:23:27 +0800

    cover test

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangxing
+ * Date: 2016/6/7
+ * Time: 14:54
+ */
+
+define("APP_ENV", "dev");
+define("APP_NAME", "Issue");
+
+require_once __DIR__ . "/../src/bootstrap.php";
